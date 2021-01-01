f9e9da1fe02bc57de1f94e79bc1ab2e327cfa2d9
Muhammad Junaid <Muhammad Junaid@DESKTOP-9P6R7E0>
2021-01-01 02:20:37 -0800

    Abc

--- JenkinsTask1/JenkinsTask.java
+++ JenkinsTask1/JenkinsTask.java
@@ -1,7 +1,7 @@
 import java.io.*;
 public class JenkinsTask {
 	public static void main(String[] args) {
-//		System.out.println("Hello this is my first jenkin projectt");
+		System.out.println("Hello this is my first jenkin projectt");
 		System.out.println("Hello this is my first jenkin statement123");
 	}
  
